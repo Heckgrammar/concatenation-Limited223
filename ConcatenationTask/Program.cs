77a27e41f0bdb99345add6b64db4c77a288cb840
Limited223 <22AAbbas@heckgrammar.co.uk>
2025-01-26 13:07:51 +0000

    Update Program.cs

--- ConcatenationTask/Program.cs
+++ ConcatenationTask/Program.cs
@@ -18,7 +18,7 @@ namespace ConcatenationTask
             // use the test data Bob, Smith, 14
             // ammend this code to output in one command
             // Hello Bob Smith you are 14 years old
-            Console.WriteLine(FirstName + ' '+ LastName + ' '+ age);
+            Console.WriteLine($"Hello {FirstName} {LastName}, you are {age} years old.");
             // save and commit your program
             // add evidence of the test input and your own input running the program
         }
